7f895d62fca65d6dfd29538e363e5cc7c878dd5d
Romain Tello <romain.tello@gmail.com>
2020-03-03 11:52:05 +0100

    Add Category page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+export { default as CategoryPage } from './Category';
 export { default as ContactPage } from './Contact';
 export { default as HomePage } from './Home';
 export { default as NotFoundPage } from './NotFound';
